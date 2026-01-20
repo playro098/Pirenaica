9814c52fc942d782f4e698a559d6accf2d975282
playro098 <guillen.pere@proton.me>
2026-01-20 13:58:22 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://pirenaica.pages.dev",
+    baseUrl: "https://pirenaica.pages.dev/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
